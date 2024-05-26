a6da3bcaa8e97b27938dce8135f74fcc05103d3c
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-05-26 16:57:14 +0700

    18. Baru Menambahkan Controllers & Views Sebagai File Kosong Sebagai Tanda Untuk Menu User & Role

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::with('role')->get();
+        return view('admin_panel.pages.users.index', compact('users'));
+    }
+}
---
+++ resources/views/admin_panel/pages/users/show.blade.php
@@ -0,0 +1,6 @@
+<!-- resources/views/pages/index.blade.php -->
+@extends('admin_panel.layouts.master')
+
+@section('title', 'Halaman Utama Data Users')
+
+@section('content')
