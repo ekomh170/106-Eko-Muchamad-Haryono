b470c3f71c60be284205e071c9c5933595787a81
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-05-26 16:45:52 +0700

    10. Menambahkan Controllers & Tampilan Untuk Menu Laporan Pada Sidebar

---
+++ resources/views/admin_panel/pages/laporan/index.blade.php
@@ -0,0 +1,16 @@
+@extends('admin_panel.layouts.master')
+
+@section('title', 'Halaman Utama Data Obat')
+
+@section('content')
+
+    <div class="container-fluid pt-4 px-4">
+        <div class="row g-4">
+
+            <div class="col-12">
+              
+            </div>
+
+        </div>
+    </div>
+@endsection
