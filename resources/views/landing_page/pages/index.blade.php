4ac9b9f04b10f1e6d73477505f0130b32e1ed127
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-06-01 17:59:30 +0700

    44. Menambahkan Desain Produk Untuk Apotik Pada Landing Page

--- resources/views/landing_page/pages/index.blade.php
+++ resources/views/landing_page/pages/index.blade.php
@@ -10,14 +10,17 @@
                 <div class="col-lg-5">
                     <div class="intro-excerpt">
                         <h1>Si ApoTrack <span class="d-block">Apotik Bogor</span></h1>
-                        <p class="mb-4">ApoTrack adalah sebuah aplikasi berbasis web yang dirancang khusus untuk mengelola sebuah apotik. Sistem Informasi apotik dalam mengelola inventaris obat, transaksi penjualan, dan manajemen data pelanggan.</p>
-                        <p><a href="{{ url('login') }}" class="btn btn-secondary me-2">Login Petugas</a><a href="{{ url('services') }}"
-                                class="btn btn-white-outline">Layanan & Obat</a></p>
+                        <p class="mb-4">ApoTrack adalah sebuah aplikasi berbasis web yang dirancang khusus untuk mengelola
+                            sebuah apotik. Sistem Informasi apotik dalam mengelola inventaris obat, transaksi penjualan, dan
+                            manajemen data pelanggan.</p>
+                        <p><a href="{{ url('login') }}" class="btn btn-secondary me-2">Login Petugas</a><a
+                                href="{{ url('services') }}" class="btn btn-white-outline">Layanan & Obat</a></p>
                     </div>
                 </div>
                 <div class="col-lg-7">
                     <div class="hero-img-wrap">
-                        <img src="{{ asset('assets/landing_page/images/desain/apotik.png') }}" class="img-fluid" style="width: 600px">
+                        <img src="{{ asset('assets/landing_page/images/desain/apotik.png') }}" class="img-fluid"
+                            style="width: 600px">
                     </div>
                 </div>
             </div>
@@ -33,7 +36,8 @@
                 <!-- Start Column 1 -->
                 <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
                     <h2 class="mb-4 section-title">Produk Kami :</h2>
-                    <p class="mb-4">Kami menyediakan berbagai macam obat-obatan berkualitas untuk kebutuhan kesehatan Anda. Temukan produk terbaik dengan harga terjangkau.</p>
+                    <p class="mb-4">Kami menyediakan berbagai macam obat-obatan berkualitas untuk kebutuhan kesehatan
+                        Anda. Temukan produk terbaik dengan harga terjangkau.</p>
                     <p><a href="{{ url('shop') }}" class="btn">Explore</a></p>
                 </div>
                 <!-- End Column 1 -->
@@ -41,8 +45,8 @@
                 <!-- Start Column 2 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
                     <a class="product-item" href="{{ url('cart') }}">
-                        <img src="{{ asset('assets/landing_page/images/product-1.png') }}"
-                            class="img-fluid product-thumbnail">
+                        <img src="{{ asset('assets/landing_page/images/obat/paracetamol-box-1.png') }}"
+                            class="img-fluid product-thumbnail" style="max-width: 100%; height: auto;">
                         <h3 class="product-title">Paracetamol</h3>
                         <strong class="product-price">Rp 5.000</strong>
 
@@ -56,8 +60,8 @@
                 <!-- Start Column 3 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
                     <a class="product-item" href="{{ url('cart') }}">
-                        <img src="{{ asset('assets/landing_page/images/product-2.png') }}"
-                            class="img-fluid product-thumbnail">
+                        <img src="{{ asset('assets/landing_page/images/obat/panadol.jpg') }}"
+                            class="img-fluid product-thumbnail" style="max-width: 100%; height: auto;">
                         <h3 class="product-title">Amoxicillin</h3>
                         <strong class="product-price">Rp 10.000</strong>
 
@@ -71,8 +75,8 @@
                 <!-- Start Column 4 -->
                 <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
                     <a class="product-item" href="{{ url('cart') }}">
-                        <img src="{{ asset('assets/landing_page/images/product-3.png') }}"
-                            class="img-fluid product-thumbnail">
+                        <img src="{{ asset('assets/landing_page/images/obat/Ibuprofen.jpg') }}"
+     class="img-fluid product-thumbnail" style="max-width: 100%; height: 300px;">
                         <h3 class="product-title">Ibuprofen</h3>
                         <strong class="product-price">Rp 8.000</strong>
 
@@ -94,7 +98,8 @@
             <div class="row justify-content-between">
                 <div class="col-lg-6">
                     <h2 class="section-title">Kenapa Kamu Harus Pilih Toko Ini?</h2>
-                    <p>Kami menyediakan layanan terbaik untuk memastikan kesehatan Anda tetap terjaga. Berikut beberapa alasan mengapa Anda harus memilih kami:</p>
+                    <p>Kami menyediakan layanan terbaik untuk memastikan kesehatan Anda tetap terjaga. Berikut beberapa
+                        alasan mengapa Anda harus memilih kami:</p>
 
                     <div class="row my-5">
                         <div class="col-6 col-md-6">
@@ -153,52 +158,49 @@
     <div class="popular-product">
         <div class="container">
             <div class="row">
-    
+
                 <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
                     <div class="product-item-sm d-flex">
                         <div class="thumbnail">
-                            <img src="{{ asset('assets/landing_page/images/product-1.png') }}" alt="Image"
+                            <img src="{{ asset('assets/landing_page/images/obat/paracetamol-box-1.png') }}" alt="Image"
                                 class="img-fluid">
                         </div>
                         <div class="pt-3">
-                            <h3>Panadol</h3>
+                            <h3>Paracetamol</h3>
                             <p>Obat pereda nyeri dan penurun demam yang efektif untuk mengatasi berbagai macam sakit.</p>
-                            <p><a href="#">Read More</a></p>
                         </div>
                     </div>
                 </div>
-    
+
                 <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
                     <div class="product-item-sm d-flex">
                         <div class="thumbnail">
-                            <img src="{{ asset('assets/landing_page/images/product-2.png') }}" alt="Image"
+                            <img src="{{ asset('assets/landing_page/images/obat/amoxicillin.jpg') }}" alt="Image"
                                 class="img-fluid">
                         </div>
                         <div class="pt-3">
                             <h3>Amoxicillin</h3>
                             <p>Antibiotik yang digunakan untuk mengobati berbagai infeksi bakteri di dalam tubuh.</p>
-                            <p><a href="#">Read More</a></p>
                         </div>
                     </div>
                 </div>
-    
+
                 <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
                     <div class="product-item-sm d-flex">
                         <div class="thumbnail">
-                            <img src="{{ asset('assets/landing_page/images/product-3.png') }}" alt="Image"
+                            <img src="{{ asset('assets/landing_page/images/obat/vitamin_c.jpg') }}" alt="Image"
                                 class="img-fluid">
                         </div>
                         <div class="pt-3">
                             <h3>Vitamin C</h3>
                             <p>Suplemen untuk meningkatkan daya tahan tubuh dan menangkal radikal bebas.</p>
-                            <p><a href="#">Read More</a></p>
                         </div>
                     </div>
                 </div>
-    
+
             </div>
         </div>
     </div>
-    
+
     <!-- End Popular Product -->
 @endsection
